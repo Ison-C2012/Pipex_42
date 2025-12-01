9899c8ba91f5f543c51aa542976a8d7e1f1c34d7
Ison Kislovodsk <ison.c.2012.tk@gmail.com>
2025-12-01 16:20:20 +0900

    pipex begins!

---
+++ proj/includes/pipex.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: keitotak <keitotak@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/01 14:58:59 by keitotak          #+#    #+#             */
+/*   Updated: 2025/12/01 16:07:10 by keitotak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "../libft/includes/libft.h"
+# include "../libft/includes/libft_bonus.h"
+# include "../libft/includes/ft_printf.h"
+
+# include <stdlib.h>
+
+void	invalid_args(void);
+
+#endif
