a61ac3c7fc9c56bec30a3be4fea46a6c337bb829
Ison Kislovodsk <ison.c.2012.tk@gmail.com>
2025-12-05 11:59:07 +0900

    rename fucn and val

--- proj/srcs/main.c
+++ proj/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: keitotak <keitotak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/01 14:56:57 by keitotak          #+#    #+#             */
-/*   Updated: 2025/12/05 01:37:25 by keitotak         ###   ########.fr       */
+/*   Updated: 2025/12/05 11:47:15 by keitotak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ int	main(int argc, char **argv, char **envp)
 	{
 		ft_putendl_fd("\e[33mInvalid arguments.", 2);
 		ft_putendl_fd("Usage: ./pipex file1 \"cmd1\" \"cmd2\" file2\e[0m", 2);
-		return (failure);
+		return (EXIT_FAILURE);
 	}
 	if (pipex(&argv[1], envp) == failure)
-		return (failure);
-	return (success);
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
 }
--- proj/srcs/wait.c
+++ proj/srcs/wait.c
@@ -6,43 +6,37 @@
 /*   By: keitotak <keitotak@student.42tokyo.jp      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/04 20:34:18 by keitotak          #+#    #+#             */
-/*   Updated: 2025/12/04 22:02:31 by keitotak         ###   ########.fr       */
+/*   Updated: 2025/12/05 11:47:02 by keitotak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	status_code(int status)
+static int	status_code(int status)
 {
 	int	code;
 
-	if (WIFEXITED(status) == true)
+	if (WIFEXITED(status))
 		code = WEXITSTATUS(status);
-	if (WIFSIGNALED(status) == true)
+	if (WIFSIGNALED(status))
 		code = WTERMSIG(status);
-	if (WIFSIGNALED(status) == true)
+	if (WIFSIGNALED(status))
 	{
 		code = WTERMSIG(status);
-		if (WCOREDUMP(status) == true)
+		if (WCOREDUMP(status))
 			return (-1);
 	}
 	return (code);
 }
 
-int	wait_for_child(t_pipex *p, pid_t pid)
+static int	wait_for_child(pid_t pid)
 {
-	int     wstatus;
+	int	wstatus;
 
-	if (waitpid(pid, &wstatus, 0) == -1)
-	{
-		close_fds(p);
+	if (waitpid(pid, &wstatus, 0) == error)
 		return (failure);
-	}
 	if (status_code(wstatus) != success)
-	{
-		close_fds(p);
 		return (failure);
-	}
 	return (success);
 }
 
@@ -51,10 +45,9 @@ int	wait_for_children(t_pipex *p)
 	int	res1;
 	int	res2;
 
-	res1 = wait_for_child(p, p->pid1);
-	res2 = wait_for_child(p, p->pid2);
+	res1 = wait_for_child(p->pid1);
+	res2 = wait_for_child(p->pid2);
 	if (res1 != success || res2 != success)
 		return (failure);
 	return (success);
 }
-
